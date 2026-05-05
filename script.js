e654047084a5a42c3f91bb261a95b1a50155ca56
Dardex7033 <r4billion@gmail.com>
2026-05-05 17:09:21 +0800

    Replace old order functions with new orderNow function

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
+<script>
+function orderNow() {
+  let message = "Hi, saya nak order makanan dari Nel's Bogorrr 🍔";
 
-function scrollToMenu() {
-  document.getElementById("menu").scrollIntoView({ behavior: "smooth" });
+  let url = "https://wa.me/601114290341?text=" + encodeURIComponent(message);
+
+  window.open(url, "_blank");
 }
-
-function openOrderNow() {
-  const modal = document.getElementById("orderModal");
-  if (modal) {
-    modal.style.display = "flex";
-  }
-}
-let selectedItem = "";
-let selectedPrice = 0;
-
-function selectItem(name, price) {
-  selectedItem = name;
-  selectedPrice = price;
-
-  document.getElementById("orderModal").style.display = "block";
-  document.getElementById("selectedItem").innerText =
-    `${name} - RM${price}`;
-}
-
-function sendWhatsApp() {
-  let qty = document.getElementById("qty").value;
-  let total = selectedPrice * qty;
-
-  let message = `Order:
-- ${selectedItem}
-- Qty: ${qty}
-- Total: RM${total}`;
-
-  window.open(
-    `https://wa.me/601114290341?text=${encodeURIComponent(message)}`
-  );
-}
+</script>
