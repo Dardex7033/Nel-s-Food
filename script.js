f94a7555408422f041727ea1b7391c405a0476c5
Dardex7033 <r4billion@gmail.com>
2026-05-12 18:10:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // OPEN ORDER PAGE (NEW PAGE SYSTEM)
-document.getElementById("openOrderPage").onclick = function () {
-  window.location.href = "order.html";
-};
+document.addEventListener("DOMContentLoaded", function () {
 
+  const btn = document.getElementById("orderBtn");
 
+  if (btn) {
+    btn.addEventListener("click", function () {
+      window.location.href = "order.html";
+    });
+  }
 
-}
+});
