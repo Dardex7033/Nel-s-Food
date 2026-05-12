16cd51b831b4c5262751444c59d331b4a1273418
Dardex7033 <r4billion@gmail.com>
2026-05-12 15:58:28 +0800

    Refactor orderNow function for better readability

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-<script>
 function orderNow() {
-  let message = "Hi, saya nak order makanan dari Nel's Bogorrr 🍔";
+  const message = "Hi, saya nak order makanan dari Nel's Bogorrr 🍔";
 
-  let url = "https://wa.me/601114290341?text=" + encodeURIComponent(message);
+  const url =
+    "https://wa.me/601114290341?text=" +
+    encodeURIComponent(message);
 
   window.open(url, "_blank");
 }
-</script>
