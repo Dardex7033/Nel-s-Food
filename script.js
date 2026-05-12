e590a0ab5ec601a41b1a8b73bf1b4902a9e331e1
Dardex7033 <r4billion@gmail.com>
2026-05-12 20:16:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-function orderNow() {
-    let name = document.getElementById("name").value;
-    let phone = document.getElementById("phone").value;
-    let location = document.getElementById("location").value;
+function openOrderForm() {
+    document.getElementById("orderPopup").style.display = "flex";
+}
 
-    let orderMessage =
-`🍔 NEW ORDER - NEL'S BOGORRR
-
-Nama: ${name}
-No Telefon: ${phone}
-
-🧾 Order Details:
-- Burger Ayam x1 - RM8
-- Fries x1 - RM5
-
-💰 Total: RM13
-
-📍 Penghantaran / Pickup: ${location}
-
-Terima kasih! 🙌`;
-
-    let whatsappURL = "https://wa.me/601114290341?text=" + encodeURIComponent(orderMessage);
-
-    window.open(whatsappURL, "_blank");
+function closeOrderForm() {
+    document.getElementById("orderPopup").style.display = "none";
 }
