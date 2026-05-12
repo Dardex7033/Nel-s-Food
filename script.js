de1802b07ce1f9e84ac57d84d72334f1e8c14a20
Dardex7033 <r4billion@gmail.com>
2026-05-12 19:50:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-// OPEN ORDER PAGE (NEW PAGE SYSTEM)
-document.addEventListener("DOMContentLoaded", function () {
+window.sendOrderWhatsApp = function () {
 
-  const btn = document.getElementById("orderBtn");
+  let name = document.getElementById("custName").value;
+  let phone = document.getElementById("custPhone").value;
+  let address = document.getElementById("custAddress").value;
 
-  if (btn) {
-    btn.addEventListener("click", function () {
-      window.location.href = "order.html";
-    });
+  if (!name || !phone || !address) {
+    alert("Sila isi semua maklumat!");
+    return;
   }
-window.open("order.html", "_blank");
-});
+
+  let msg = "";
+
+  // HEADER
+  msg += "🧾 *NEL'S BOGORRR RECEIPT*%0A";
+  msg += "----------------------------%0A";
+
+  // CUSTOMER INFO
+  msg += `👤 Name: ${name}%0A`;
+  msg += `📱 Phone: ${phone}%0A`;
+  msg += `🏠 Address: ${address}%0A`;
+  msg += "----------------------------%0A";
+
+  msg += "🍔 *ITEMS*%0A";
+
+  let total = 0;
+
+  // ITEMS
+  cart.forEach(item => {
+
+    let itemTotal = item.price * item.qty;
+    total += itemTotal;
+
+    msg += `- ${item.name} x${item.qty}   RM${itemTotal}%0A`;
+  });
+
+  msg += "----------------------------%0A";
+
+  // TOTAL
+  msg += `💰 *TOTAL: RM${total.toFixed(2)}*%0A`;
+  msg += "----------------------------%0A";
+  msg += "🙏 Thank you for ordering!%0A";
+
+  // SEND
+  window.open(
+    "https://wa.me/601114290341?text=" + encodeURIComponent(msg),
+    "_blank"
+  );
+};
