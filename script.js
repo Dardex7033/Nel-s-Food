9f968eb392219d1637923d5f13ed8e2df8a21613
Dardex7033 <r4billion@gmail.com>
2026-05-12 17:25:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,5 @@ document.getElementById("openOrderPage").onclick = function () {
 };
 
 
-// OPTIONAL: direct WhatsApp quick order (if you still want it)
-function orderNow() {
-  const message = "Hi, saya nak order makanan dari Nel's Bogorrr 🍔";
 
-  const url = "https://wa.me/601114290341?text=" + encodeURIComponent(message);
-
-  window.open(url, "_blank");
 }
